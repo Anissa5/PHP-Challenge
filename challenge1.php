19f9bac49b1348c56fd7fef2acb2deabed70106d
Anissa Harouat <anissniss75@gmail.com>
2026-08-05 11:53:26 +0200

    challenge1

---
+++ challenge1.php
@@ -0,0 +1,16 @@
+<?php
+
+$title = "Voyage avec un âne dans les Cévennes"; //string
+$lecture = false; //boolean
+$yearOfPublication = 2025; //integer
+$price = 5.98; //float
+
+echo $title;
+echo "<br>";
+echo $lecture;
+echo "<br>";
+echo $yearOfPublication;
+echo "<br>";
+echo $price;
+
+?>
